6c54e3d3a46d6e39f440856bf213c6f1904cbdfe
artem <artemtotal@Gmail.com>
2025-02-06 12:57:41 +0100

    Initial commit

---
+++ infra/variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-099da3ad959447ffa"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "eu-central-1a"
+}
+
+variable "availability_zone_2" {
+  description = "Zweite Availability Zone für Load Balancer"
+  type        = string
+  default     = "eu-central-1b"  
+}
+
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the SSH key pair to access the instance"
+  type        = string
+  default     = "c-a"  
+}
+# variable "aws_access_key" {
+#   default = "ASIAVA5YK74JKRVCCVKV"
+# }
+
+# variable "aws_secret_key" {
+#   default = "H/jbQj+QH1byS39GOkcA5ZVDfxvC7BiLXDjVvBnR"
+# }
+
