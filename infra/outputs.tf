e8115e31f44457ad541b3f27fd4aeaf1bf5922fb
artem <artemtotal@Gmail.com>
2025-02-18 17:34:41 +0100

    1 instance

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,11 @@
-output "load_balancer_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.app_lb.dns_name
-}
-output "acm_certificate_arn" {
-  value = aws_acm_certificate.cert.arn
-}
+# output "load_balancer_dns_name" {
+#   description = "DNS name of the Application Load Balancer"
+#   value       = aws_lb.app_lb.dns_name
+# }
+# output "acm_certificate_arn" {
+#   value = aws_acm_certificate.cert.arn
+# }
 
-output "acm_certificate_domain_validation_options" {
-  value = aws_acm_certificate.cert.domain_validation_options
-}
+# output "acm_certificate_domain_validation_options" {
+#   value = aws_acm_certificate.cert.domain_validation_options
+# }
